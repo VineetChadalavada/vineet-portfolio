b8faafdf80fa43ce550ac272e76293f97ad3dee9
VineetChadalavada <vchadala@charlotte.edu>
2025-10-29 14:43:34 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,40 +2,27 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es6: true
+    es6: true,
+    browser: true,
   },
-  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
-  ignorePatterns: ["node_modules/*", ".next/*", ".out/*"], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ["eslint:recommended"],
+  parserOptions: { ecmaVersion: 8, sourceType: 'module' },
+  ignorePatterns: ['node_modules/*', '.next/*', '.out/*'],
   overrides: [
-    // This configuration will apply only to TypeScript files
     {
-      files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-      parser: "@typescript-eslint/parser",
-      settings: { react: { version: "detect" } },
-      env: {
-        browser: true,
-        node: true,
-        es6: true
-      },
+      files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+      parser: '@typescript-eslint/parser',
+      settings: { react: { version: 'detect' } },
       extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended" // TypeScript rules
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'next',
+        'next/core-web-vitals',
       ],
       rules: {
-        // We will use TypeScript's types for component props instead
-        "react/prop-types": "off",
-
-        // I suggest this setting for requiring return types on functions only where useful
-        "@typescript-eslint/explicit-function-return-type": [
-          "warn",
-          {
-            allowExpressions: true,
-            allowConciseArrowFunctionExpressionsStartingWithVoid: true
-          }
-        ]
+        // Disable return-type warnings and other noisy rules
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'react/prop-types': 'off',
       },
-      parserOptions: { sourceType: "module" }
-    }
-  ]
+    },
+  ],
 };
