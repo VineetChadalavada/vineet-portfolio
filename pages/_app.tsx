6b6299bb1c2bdfce2bd6a7edf32477d20a864651
VineetChadalavada <vchadala@charlotte.edu>
2025-10-29 14:55:44 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,14 @@
-import { useEffect } from 'react';
-import App, { AppProps } from 'next/app';
-import { ChakraProvider, Box } from '@chakra-ui/react';
-import AppLayout from 'components/layouts/appLayout';
-import { PrismGlobal } from 'components/theme/prism';
-import { useRouter } from 'next/router';
-import * as gtag from 'lib/gtag';
-import { AnimatePresence } from 'framer-motion';
-import { theme } from 'components/theme';
-import { AccentGlobal } from 'components/theme/Accent';
-import { FontsGlobal } from 'components/theme/fonts';
-import { Analytics } from '@vercel/analytics/react';
+import { ChakraProvider, Box } from '@chakra-ui/react'
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { theme } from 'components/theme'
+import { AccentGlobal } from 'components/theme/Accent'
+import { FontsGlobal } from 'components/theme/fonts'
+import { PrismGlobal } from 'components/theme/prism'
+import { Analytics } from '@vercel/analytics/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
 
   return (
     <ChakraProvider theme={theme} resetCSS={true}>
@@ -29,24 +16,17 @@ function MyApp({ Component, pageProps }: AppProps) {
       <AccentGlobal />
       <PrismGlobal />
       <Analytics />
-      <AppLayout>
-        <AnimatePresence
-          // exitBeforeEnter
-          mode='wait'
-          initial={false}
-          onExitComplete={() => window.scrollTo(0, 0)}
-        >
-          <Box key={router.route}>
-            <Component {...pageProps} />
-          </Box>
-        </AnimatePresence>
-      </AppLayout>
+      <AnimatePresence
+        mode="wait"
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Box key={router.route}>
+          <Component {...pageProps} />
+        </Box>
+      </AnimatePresence>
     </ChakraProvider>
-  );
+  )
 }
 
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  return { ...appProps };
-};
-export default MyApp;
+export default MyApp
