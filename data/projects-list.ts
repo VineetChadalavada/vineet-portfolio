aa72f2caefe232feeb4a2b7519311130516a5eeb
VineetChadalavada <vchadala@charlotte.edu>
2025-10-29 13:03:31 -0400

    projects-list.ts

--- data/projects-list.ts
+++ data/projects-list.ts
@@ -1,63 +1,23 @@
-// import peterpitch from "assets/images/projects/pp.png";
-// import notch from "assets/images/projects/notch.png";
-// import workof from "assets/images/projects/workof.png";
-// import souq from "assets/images/projects/souq.png";
-// import gosave from "assets/images/projects/gosave.png";
-
 export const projectsList = [
   {
-    id: 1,
-    title: "Peterpitch",
-    // logo: peterpitch,
-    logo: '/assets/images/projects/pp.png',
-    blurHash: "L4ADc400P*Zi4Tu1y;Qo00pH#YXl",
-    link: "https://peterpitch.com/",
-    desc: `Peterpitch is a tool that takes the fun in your company seriously and turns your meetings into real performance boosters. With Peterpitch, you round up the pack of employees and turn even the shakiest puppies into true wolves.`,
-    technologies: ["rails", "react", "javascript"]
+    name: "Multi-Processor Cache Coherence Architecture",
+    description:
+      "Implemented 4-way MESI protocol for inter-core cache consistency using C and RISC-V architecture simulation.",
+    link: "https://github.com/VineetChadalavada",
   },
   {
-    id: 2,
-    title: "Notch",
-    link: "https://www.hellonotch.com/",
-    // logo: notch,
-    logo: '/assets/images/projects/notch.png',
-    blurHash: "L0Aer?tjH[tPyAayj[j[00ay%xkB",
-    desc: `This is a project management website for design and procurement teams. 
-    It helps to collaborate and manage all of your floor plans in one place, writing specs, budgeting and timelines. 
-    This project management website provides a wide variety of features like timeline tracking, spec sheet generation, integrated budget, vendor + inventory management, user usage tracking and many more.`,
-    technologies: ["ruby", "rails", "javascript"]
+    name: "UART Interface with Classification Engine",
+    description:
+      "Designed UART TX/RX interface integrated with ML classifier in VHDL on Xilinx Vivado for secure serial communication.",
   },
   {
-    id: 3,
-    title: "Workof Ecommerce",
-    // logo: workof,
-    logo: '/assets/images/projects/workof.png',
-    blurHash: "L2M5%%0000EN00I:-oIp00kC?^sl",
-    link: "https://workof.com/",
-    desc: `This is an Ecommerce furniture site. It provides a single platform to multiple vendors where they can put their products on sale.`,
-    technologies: ["ruby", "rails", "javascript"]
+    name: "Predictive Maintenance Alert System",
+    description:
+      "Developed an ML-based alerting system using NASA’s C-MAPSS dataset to predict engine failure and generate alerts.",
   },
   {
-    id: 4,
-    title: "Souq of Qatar",
-    // logo: souq,
-    logo: '/assets/images/projects/souq.png',
-    blurHash: "L1O^uE0679I00AGT?.H{00y7p2tK",
-    link: "https://www.souqofqatar.com/",
-    desc: `Souq of Qatar is online comparison website. This website is independent of any retailers, it displays the products of all the major stores.
-    Just like when you compare prices and products while visiting physical shops and stores, online price comparison behaves in the same way. 
-    It is an established method in the West for people to visit the online comparison websites first whenever they are in need to buy something. 
-    This provides the benefits of convenience, choice, and price efficiency.`,
-    technologies: ["ruby", "rails", "javascript"]
+    name: "Agro Drone",
+    description:
+      "Built an ARM Cortex-M4-based agricultural drone using ArduPilot for precision crop analysis and spraying automation.",
   },
-  {
-    id: 5,
-    title: "GoSave",
-    // logo: gosave,
-    logo: '/assets/images/projects/gosave.png',
-    blurHash: "L09sWSxdH;s?%6axWAfk00V?%jax",
-    link: "https://gosavehq.com/",
-    desc: `The GoSave app let's you send allowances, assign chores, and approve spending all from your smartphone. These actions appear on each kid's toy, along with a live bank balance so they can watch their savings grow.`,
-    technologies: ["ruby", "rails"]
-  }
 ];
