26711e8cdc6e3f7d753d38d9f9a80faa3e045d94
VineetChadalavada <vchadala@charlotte.edu>
2025-12-03 10:26:24 -0500

    Update Publications.tsx

--- components/Publications.tsx
+++ components/Publications.tsx
@@ -23,42 +23,49 @@ export default function Publications() {
           </Text>
 
           <List spacing={5} fontSize="md">
-            <ListItem>
-              <b>V. Chadalavada</b>, F. Saqib,{" "}
-              “<i>Side-Channel Attacks in FPGA-Based Cloud Environments: Evaluating Prime+Probe Attacks</i>,”
-              accepted at <b>PAINE 2025</b>.
-            </ListItem>
 
-      
+            {/* 1. DDR4 Timing Attack */}
             <ListItem>
               <b>V. Chadalavada</b>, N. Lodge, D. Aklekar, F. Saqib,{" "}
-              “<i>Timing-Based Side-Channel Attack on DDR4 Memory in FPGA Cloud Environments</i>.”
+              “<i>Timing-Based Side-Channel Attack on DDR4 Memory in FPGA Cloud Environments</i>.” 
             </ListItem>
 
+            {/* 2. DDR4 Countermeasures */}
             <ListItem>
-              D. Aklekar, <b>V. Chadalavada</b>, N. Tambe, F. Saqib,{" "}
-              “<i>Multi-Granular Information Flow Tracking in RISC-V based SoCs</i>.”
+              <b>V. Chadalavada</b>, N. D. Tambe, D. Aklekar, F. Saqib,{" "}
+              “<i>Mitigating DDR4 Timing Side-Channels in FPGA Cloud Platforms</i>.”
             </ListItem>
 
+            {/* 3. GLIFT for 3PIPs */}
             <ListItem>
               D. Aklekar, <b>V. Chadalavada</b>, N. Lodge, F. Saqib,{" "}
               “<i>Gate-Level Information Flow Tracking for Securing 3PIPs</i>.”
             </ListItem>
 
+            {/* 4. Multi-Granular IFT */}
+            <ListItem>
+              D. Aklekar, <b>V. Chadalavada</b>, N. Tambe, F. Saqib,{" "}
+              “<i>Multi-Granular Information Flow Tracking in RISC-V based SoCs</i>.”
+            </ListItem>
+
+            {/* 5. ML-Based Dynamic Logic Locking */}
             <ListItem>
               N. D. Tambe, N. Lodge, <b>V. Chadalavada</b>, F. Saqib,{" "}
               “<i>Implementation of Machine Learning-Based Dynamic Logic Locking</i>.”
             </ListItem>
 
+            {/* 6. Federated Learning Counterfeit IC Detection */}
             <ListItem>
               N. Lodge, <b>V. Chadalavada</b>, N. Tambe, M. Y. A. Raja, F. Saqib,{" "}
               “<i>Counterfeit IC Detection via Federated Learning</i>.”
             </ListItem>
 
+            {/* 7. Hexacopter Paper (as-is) */}
             <ListItem>
               <b>V. Chadalavada</b> et al.,{" "}
               “<i>Design and Development of a Hexacopter for Fertilizer Spraying in Agricultural Fields</i>.”
             </ListItem>
+
           </List>
 
           <Text fontSize="sm" color="gray.500" pt={6}>
